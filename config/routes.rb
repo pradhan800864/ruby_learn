e528e3d735e564db68f86d6b9388a430e36953d6
ppattepu <ppattepu@voceracommunications.com>
2024-06-27 01:26:47 +0530

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root 'pages#home'
+  get 'about', to: 'pages#about'
 
   # Defines the root path route ("/")
   # root "posts#index"
